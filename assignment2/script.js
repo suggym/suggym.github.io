892288087c371ea483d7c5feb3108b066a8cb3d9
suggym <miasug05@gmail.com>
2026-05-08 09:56:23 +1000

    updates

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,21 +1,44 @@
-const video = document.querySelector("#custom-video-player");
+const audio = document.querySelector("#custom-audio-player");
 const playPauseBtn = document.querySelector("#play-pause-btn");
+const mutedBtn = document.querySelector("#muted-btn");
 const playPauseImg = document.querySelector("#play-pause-img");
+const mutedImg = document.querySelector("#muted-img");
 const progressBar = document.querySelector("#progress-bar-fill");
-video.removeAttribute("controls");
-// playPauseBtn.addEventListener("click", togglePlayPause);
-video.addEventListener("timeupdate", updateProgressBar);
+audio.removeAttribute("controls");
+playPauseBtn.addEventListener("click", togglePlayPause);
+mutedBtn.addEventListener("click", toggleMute);
+audio.addEventListener("timeupdate", updateProgressBar);
 function togglePlayPause() {
-  if (video.paused || video.ended) {
-    video.play();
-    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
+  if (audio.paused == true || audio.ended == true) {
+    audio.play();
+    playPauseImg.src =
+      "https://img.icons8.com/?size=100&id=Wf72tFY1MCeV&format=png&color=000000";
   } else {
-    video.pause();
-    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+    audio.pause();
+    playPauseImg.src =
+      "https://img.icons8.com/?size=100&id=5GnFrcQmc9iB&format=png&color=000000";
+  }
+}
+
+function toggleMute() {
+  if (audio.muted == true) {
+    audio.muted = false;
+    mutedImg.src =
+      "https://img.icons8.com/?size=100&id=IlvXrZQW2plu&format=png&color=000000";
+  } else {
+    audio.muted = true;
+    mutedImg.src =
+      "https://img.icons8.com/?size=100&id=K2qo7aCu72SI&format=png&color=000000";
   }
 }
 function updateProgressBar() {
-  const value = (video.currentTime / video.duration) * 100;
+  const value = (audio.currentTime / audio.duration) * 100;
   progressBar.style.width = value + "%";
 }
 // Add other functionalities here
+// pause play etc, in html where button is styled and in a flexbox: <button id="pause-button">pause audio</button>  <button id="play-button">play audio</button>
+//in js const pauseButton = document.querySelector("#pause-button"); console.log (pause)
+//cont. pauseButton.addEventListener("click", pauseAudio);
+//cont. function pauseAudio() {airportAudio.pause();}
+//access element, event listener, function
+//https://archive.org/details/studyvid
